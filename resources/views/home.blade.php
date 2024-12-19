f190a928fca21e7f89088cc13443cddfcead6000
wekauwau <wekauwau@gmail.com>
2024-12-19 18:25:51 +0700

    move

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto py-24 sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+                <x-home.welcome />
                 <x-home.activities-section />
             </div>
         </div>
