92bb27ab0a92a062336a88780ffe101dc2f5c865
wekauwau <wekauwau@gmail.com>
2024-12-19 08:30:57 +0700

    revert User

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,16 +12,15 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
     protected $fillable = [
+        'name',
         'email',
         'password',
-        'student_id',
-        'name',
-        'cohort',
-        'address',
-        'gender',
-        'pfp',
-        'join_date',
     ];
 
     /**
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('student_id');
-            $table->string('name');
-            $table->integer('cohort');
-            $table->string('address');
-            $table->boolean('gender');  // 0 female
-            $table->string('pfp');
-            $table->date('join_date');
             $table->rememberToken();
             $table->timestamps();
         });
