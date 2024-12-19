c0c2dfd3cd019e9cb272f2e4d5afd7ef8e867637
wekauwau <wekauwau@gmail.com>
2024-12-19 10:58:58 +0700

    add info

--- database/seeders/MemberSeeder.php
+++ database/seeders/MemberSeeder.php
@@ -10,6 +10,8 @@ class MemberSeeder extends Seeder
     public function run(): void
     {
         Member::factory(46)->create();
+        // INFO:  1 -  6 = pengurus harian
+        // INFO:  7 - 46 = pengurus divisi
 
         $base = '24';
         for ($i = 1; $i < 47; $i++) {
