cf547ebdd36942d44903f047b10b33e22200a939
wekauwau <wekauwau@gmail.com>
2024-12-19 11:27:09 +0700

    Division

--- database/seeders/DivisionSeeder.php
+++ database/seeders/DivisionSeeder.php
@@ -9,18 +9,17 @@ class DivisionSeeder extends Seeder
 {
     public function run(): void
     {
-        // TODO: leader_member_id
         $data = [
-            ['Jurnalistik'],
-            ['HRD'],
-            ['Humas'],
-            ['Kaderisasi'],
-            ['Orsen'],
-            ['Media'],
-            ['P2'],
-            ['Danus'],
-            ['Kesravo'],
-            ['IT'],
+            ['Jurnalistik', 7],
+            ['HRD', 11],
+            ['Humas', 15],
+            ['Kaderisasi', 19],
+            ['Orsen', 23],
+            ['Media', 27],
+            ['P2', 31],
+            ['Danus', 35],
+            ['Kesravo', 39],
+            ['IT', 43],
         ];
 
         foreach ($data as $item) {
