7ecd8ee019eff27399063142bfbfa01e3e6f3c2b
wekauwau <wekauwau@gmail.com>
2024-12-19 13:20:10 +0700

    fix

--- database/seeders/MemberSeeder.php
+++ database/seeders/MemberSeeder.php
@@ -9,16 +9,14 @@ class MemberSeeder extends Seeder
 {
     public function run(): void
     {
-        Member::factory(46)->create();
         // INFO:  1 -  6 = pengurus harian
         // INFO:  7 - 46 = pengurus divisi
 
         $base = '24';
-        for ($i = 1; $i < 47; $i++) {
-            Member::find($i)
-                ->update([
-                    'student_id' => $base + sprintf('%02d', $i),
-                ]);
+        for ($i = 1; $i <= 47; $i++) {
+            Member::factory(1)->create([
+                'student_id' => $base.sprintf('%02d', $i),
+            ]);
         }
     }
 }
