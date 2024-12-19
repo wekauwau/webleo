3057aa5c8938f13f7c9ae4d54e5b1ecf02d959bd
wekauwau <wekauwau@gmail.com>
2024-12-19 18:09:33 +0700

    change ImageSeeder

--- database/seeders/ImageSeeder.php
+++ database/seeders/ImageSeeder.php
@@ -10,15 +10,23 @@ class ImageSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            ['kegiatan-00.jpg', ''],
-            ['kegiatan-01.jpg', ''],
-            ['kegiatan-02.jpg', ''],
+            ['kegiatan-00.jpg'],
+            ['kegiatan-01.jpg'],
+            ['kegiatan-02.jpg'],
+
+            ['wawasan-00.jpg'],
+            ['wawasan-01.jpg'],
+            ['wawasan-02.jpg'],
+
+            ['berita-01.webp'],
+            ['berita-01.jpg'],
+            ['berita-02.webp'],
         ];
 
         foreach ($data as $item) {
             Image::create([
                 'name' => $item[0],
-                'alt' => $item[1],
+                'alt' => 'YNKTS',
             ]);
         }
     }
