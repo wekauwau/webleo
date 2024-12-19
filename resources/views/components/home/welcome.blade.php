5635fe4f64c6c937c0f032ad16bf7c3a84d6fee6
wekauwau <wekauwau@gmail.com>
2024-12-19 18:59:15 +0700

    change

--- resources/views/components/home/welcome.blade.php
+++ resources/views/components/home/welcome.blade.php
@@ -1,6 +1,7 @@
 <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
     <h1 class="text-2xl font-medium text-gray-900">
-        Selamat datang di Web Himpunan Mahasiswa Matematika UWU
+        Selamat datang di website Himpunan Mahasiswa Matematika<br/>
+        Universitas Winchester United
     </h1>
 
     <p class="mt-6 text-gray-500 leading-relaxed">
