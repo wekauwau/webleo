d50e4a3a6afebb30740a6e7eb127fb977e80048f
wekauwau <wekauwau@gmail.com>
2024-12-19 23:05:02 +0700

    revert

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -29,13 +29,7 @@
 
         <!-- Page Content -->
         <main>
-            <div class="py-12">
-                <div class="max-w-7xl mx-auto py-24 sm:px-6 lg:px-8">
-                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                        {{ $slot }}
-                    </div>
-                </div>
-            </div>
+            {{ $slot }}
         </main>
 
         <x-layouts.footer />
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,12 @@
 <x-app-layout>
-    <x-home.welcome />
-    <x-home.publications-section publication-type-id="1" more="kegiatan" />
-    <x-home.publications-section publication-type-id="2" more="wawasan" />
-    <x-home.publications-section publication-type-id="3" more="berita" />
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto py-24 sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-home.welcome />
+                <x-home.publications-section publication-type-id="1" more="kegiatan" />
+                <x-home.publications-section publication-type-id="2" more="wawasan" />
+                <x-home.publications-section publication-type-id="3" more="berita" />
+            </div>
+        </div>
+    </div>
 </x-app-layout>
