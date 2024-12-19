2b0406bcbe44d78a4b1fe95602bc9e4c2de0bb01
wekauwau <wekauwau@gmail.com>
2024-12-19 20:16:20 +0700

    ref about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,25 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto py-24 sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-home.welcome />
+                <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+                    <h1 class="mb-4 text-4xl font-extrabold text-center leading-none tracking-tight text-gray-800 md:text-5xl lg:text-4xl dark:text-white">
+                        Visi
+                    </h1>
+                    <p class="mb-6 text-lg font-normal text-gray-600 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id finibus diam. Fusce ac efficitur magna, eu bibendum libero. Mauris interdum tellus felis, ut rhoncus massa ornare eget. Curabitur eget venenatis orci.
+                    </p>
+                    <hr class="w-48 h-1 mx-auto my-4 bg-gray-800 border-0 rounded md:my-10 dark:bg-gray-700">
+                    <h1 class="mb-4 text-4xl font-extrabold text-center leading-none tracking-tight text-gray-800 md:text-5xl lg:text-4xl dark:text-white">
+                        Misi
+                    </h1>
+                    <div class="mb-6 text-lg font-normal text-gray-600 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+                        <ol class="mt-2 space-y-1 list-decimal list-inside">
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                            <li>Mauris id finibus diam. Fusce ac efficitur magna, eu bibendum libero.</li>
+                            <li>Mauris interdum tellus felis, ut rhoncus massa ornare eget. Curabitur eget venenatis orci.</li>
+                        </ol>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
