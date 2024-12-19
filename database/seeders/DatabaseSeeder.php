3dfcfae05c4cd1cfc4a251d08f1b7a01afbd7c55
wekauwau <wekauwau@gmail.com>
2024-12-19 11:37:55 +0700

    Position

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
             MemberSeeder::class,
             ExecutiveSeeder::class,
             DivisionSeeder::class,
+            PositionSeeder::class,
         ]);
     }
 }
--- database/seeders/PositionSeeder.php
+++ database/seeders/PositionSeeder.php
@@ -9,10 +9,23 @@ class PositionSeeder extends Seeder
 {
     public function run(): void
     {
-        // TODO: finish
         $data = [
-            ['Ketua'],
+            // Pengurus harian
+            [1, 1, null],
+            [2, 2, null],
+            [3, 3, null],
+            [4, 4, null],
+            [5, 5, null],
+            [6, 6, null],
         ];
+
+        $divisionId = 1; // Start with division ID 1
+        for ($i = 7; $i <= 46; $i++) {
+            $data[] = [$i, null, $divisionId];
+            if (($i - 6) % 4 === 0) { // Every 4 entries, increment the division ID
+                $divisionId++;
+            }
+        }
 
         foreach ($data as $item) {
             Position::create([
