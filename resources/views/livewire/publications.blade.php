3cdf63132710098c2ebd435cbd22669a820a8ad2
wekauwau <wekauwau@gmail.com>
2024-12-20 00:50:15 +0700

    fix links

--- resources/views/livewire/publications.blade.php
+++ resources/views/livewire/publications.blade.php
@@ -3,11 +3,11 @@
     @foreach ($posts as $post)
         <article class="flex flex-col shadow my-4">
             <!-- Article Image -->
-            <a href="#" class="h-52 overflow-y-hidden hover:opacity-75">
+            <a href="{{ route('post', ['id' => $post->id]) }}" class="h-52 overflow-y-hidden hover:opacity-75">
                 <img src="{{ route('image', [$post->image->name]) }}">
             </a>
             <div class="bg-white flex flex-col justify-start p-6">
-                <a href="#" class="text-xl font-bold hover:text-gray-700">
+                <a href="{{ route('post', ['id' => $post->id]) }}" class="text-xl font-bold hover:text-gray-700">
                     {{ $post->title }}
                 </a>
                 <p class="text-sm pb-3">
@@ -16,7 +16,7 @@
                 <p class="mb-6 overflow-hidden line-clamp-3 overflow-ellipsis">
                     {{ $post->content }}
                 </p>
-                <a href="#" class="uppercase text-gray-800 hover:text-black">
+                <a href="{{ route('post', ['id' => $post->id]) }}" class="uppercase text-gray-800 hover:text-black">
                     lanjutkan membaca <i class="fas fa-arrow-right"></i>
                 </a>
             </div>
